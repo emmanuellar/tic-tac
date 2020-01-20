2f961a3b04e0d285a4db8805fcf251f7e1abdab6
emmanuellar <emmanuellar805@gmail.com>
2020-01-21 00:40:45 +0100

    forgot to commit, damn

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ function Square(props){
 
     handleClick(i) {
       const squares =this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]){
+        return;
+      }
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         squares: squares,
@@ -36,8 +39,15 @@ function Square(props){
     }
   
     render() {
-      const status = 'Next player: ' + 
-      (this.state.xIsnext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner){
+        status =  'Winner: ' + winner;
+      } else {
+        const status = 'Next player: ' + 
+        (this.state.xIsnext ? 'X' : 'O');
+      }
+
   
       return (
         <div>
@@ -78,7 +88,25 @@ function Square(props){
     }
   }
   
-
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   // ========================================
   
   ReactDOM.render(
